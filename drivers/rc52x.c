4fa355050544b0a8fe4692921b8230b02477fb0c
André van Schoubroeck <andre@philosopher.it>
2020-12-02 20:38:59 +0100

    It's not working, and I'm not sure what I am doing different from a known working implementation

--- drivers/rc52x.c
+++ drivers/rc52x.c
@@ -25,7 +25,9 @@ int rc52x_get_chip_version(rc52x_t *rc52x, uint8_t* chip_id) {
 
 int rc52x_init(rc52x_t *rc52x) {
 	// For initial testing, a sequence of commands found
-	// in common implementations
+	// in a working implementation
+	//rc52x_set_reg8(rc52x, MFRC_REG_CommandReg, MFRC_SoftReset);
+	HAL_Delay(100);
 
 	rc52x_set_reg8(rc52x, MFRC_REG_TModeReg, 0x8D);
 	rc52x_set_reg8(rc52x, MFRC_REG_TPrescalerReg, 0x3E);
@@ -34,65 +36,118 @@ int rc52x_init(rc52x_t *rc52x) {
 	rc52x_set_reg8(rc52x, MFRC_REG_RFCfgReg, 0x70);			// 48dB gain
 	rc52x_set_reg8(rc52x, MFRC_REG_TxASKReg, 0x40);
 	rc52x_set_reg8(rc52x, MFRC_REG_ModeReg, 0x3D);
+
+
 	rc52x_or_reg8(rc52x, MFRC_REG_TxControlReg, 0x03); // Antenna On
 }
 
-int rc52x_anticol(rc52x_t *rc52x, uint8_t * buffer) {
 
-	rc52x_set_reg8(rc52x,MFRC_REG_BitFramingReg, 0x07);
-	buffer[0] =  PICC_CMD_REQA;
-	rc52x_set_reg8(rc52x,MFRC_REG_ComlEnReg,  0x80);
-	rc52x_and_reg8(rc52x,MFRC_REG_ComIrqReg,~0x80);
-	rc52x_or_reg8(rc52x,MFRC_REG_FIFOLevelReg, 0x80);
+// Some defines from a known good implmementation
+#define MI_OK			0
+#define MI_NOTAGERR		1
+#define MI_ERR			2
+#define MFRC522_MAX_LEN					16				// Buf len byte
+
+uint8_t MFRC522_ToCard(rc52x_t *rc52x, uint8_t command, uint8_t * sendData, uint8_t sendLen, uint8_t * backData, uint16_t * backLen) {
+
+
+	uint8_t status = MI_ERR;
+	uint8_t irqEn = 0x00;
+	uint8_t waitIRq = 0x00;
+	uint8_t lastBits;
+	uint8_t n;
+	uint16_t i;
+
+	switch (command) {
+		case MFRC_MFAuthent: {
+			irqEn = 0x12;
+			waitIRq = 0x10;
+			break;
+		}
+		case MFRC_Transceive: {
+			irqEn = 0x77;
+			waitIRq = 0x30;
+			break;
+		}
+		default:
+		break;
+	}
+
+	rc52x_set_reg8(rc52x,MFRC_REG_ComlEnReg,  irqEn | 0x80);
+	//rc52x_and_reg8(rc52x,MFRC_REG_ComIrqReg, ~0x80);
+	rc52x_set_reg8(rc52x,MFRC_REG_ComIrqReg,0x00);
+	rc52x_or_reg8(rc52x,MFRC_REG_FIFOLevelReg,  0x80);
 	rc52x_set_reg8(rc52x,MFRC_REG_CommandReg, MFRC_Idle);
-	mfrc522_send(rc52x,MFRC_REG_FIFODataReg,buffer, 1);
-	rc52x_set_reg8(rc52x,MFRC_REG_CommandReg, MFRC_Transceive);
 
-	rc52x_or_reg8(rc52x,MFRC_REG_BitFramingReg, 0x80);
+	// Writing data to the FIFO
+	mfrc522_send(rc52x,MFRC_REG_FIFODataReg,sendData, sendLen);
 
+	// Execute the command
+	rc52x_set_reg8(rc52x,MFRC_REG_CommandReg, command);
+	if (command == MFRC_Transceive)
+		rc52x_or_reg8(rc52x,MFRC_REG_BitFramingReg, 0x80);					// StartSend=1,transmission of data starts
 
-	delay_ms(100);
-	rc52x_and_reg8(rc52x,MFRC_REG_BitFramingReg, ~0x80);
+	// Waiting to receive data to complete
+	i = 2000;	// i according to the clock frequency adjustment, the operator M1 card maximum waiting time 25ms
+	do {
+		// CommIrqReg[7..0]
+		// Set1 TxIRq RxIRq IdleIRq HiAlerIRq LoAlertIRq ErrIRq TimerIRq
+		//n = MFRC522_ReadRegister(MFRC522_REG_COMM_IRQ);
+		rc52x_get_reg8(rc52x,MFRC_REG_ComIrqReg, &n);
+		i--;
+	} while ((i!=0) && !(n&0x01) && !(n&waitIRq));
 
+	rc52x_and_reg8(rc52x,MFRC_REG_BitFramingReg, ~0x80);												// StartSend=0
 
+	if (i != 0)  {
+		uint8_t error;
+		rc52x_get_reg8(rc52x,MFRC_REG_ErrorReg, &error);
+		if (!(error & 0x1B)) {
+			status = MI_OK;
+			if (n & irqEn & 0x01) status = MI_NOTAGERR;
+			if (command == MFRC_Transceive) {
+				//n = MFRC522_ReadRegister(MFRC522_REG_FIFO_LEVEL);
+				rc52x_get_reg8(rc52x,MFRC_REG_FIFOLevelReg, &n);
+				//lastBits = MFRC522_ReadRegister(MFRC522_REG_CONTROL) & 0x07;
+				rc52x_get_reg8(rc52x,MFRC_REG_ControlReg, &lastBits);
+				lastBits &= 0x07;
+
+				if (lastBits) *backLen = (n - 1) * 8 + lastBits; else *backLen = n * 8;
+				if (n == 0) n = 1;
+				if (n > MFRC522_MAX_LEN) n = MFRC522_MAX_LEN;
+				//for (i = 0; i < n; i++) backData[i] = MFRC522_ReadRegister(MFRC522_REG_FIFO_DATA);		// Reading the received data in FIFO
+				mfrc522_recv(rc52x,MFRC_REG_FIFODataReg, backData, n);
+
+			}
+		} else status = MI_ERR;
+	}
+	return status;
+}
+
+int rc52x_anticol(rc52x_t *rc52x, uint8_t * serNum) {
+	// FOr initial testing, function ported from known wokring implementaiton
+	// However, we do not get past the REQA
+
+	uint8_t status = MI_OK;
+	uint8_t i;
+	uint8_t serNumCheck = 0;
+	uint16_t unLen;
+
+	rc52x_set_reg8(rc52x,MFRC_REG_BitFramingReg, 0x07);												// TxLastBists = BitFramingReg[2..0]
+	serNum[0] = PICC_CMD_REQA;
+	status = MFRC522_ToCard(rc52x, MFRC_Transceive, serNum, 1, serNum, &unLen);
+	if ((status != MI_OK) || (unLen != 0x10)) status = MI_ERR;
+
+	if (status) return status;
 
 	rc52x_set_reg8(rc52x,MFRC_REG_BitFramingReg, 0x00);
-
-	// Let's start simple and follow the implementation by
-	// https://github.com/song940/RFID-RC522
-	// Later we'll move on to a complete anti collision and
-	// cascading implementation like is done in
-	// https://github.com/miguelbalboa/rfid/
-	buffer[0] = PICC_CMD_SEL_CL1;
-	buffer[1] = 0x20;
-
-	rc52x_set_reg8(rc52x,MFRC_REG_ComlEnReg, 0x77 | 0x80);
-	rc52x_and_reg8(rc52x,MFRC_REG_ComIrqReg,~0x80);
-	rc52x_or_reg8(rc52x,MFRC_REG_FIFOLevelReg, 0x80);
-	rc52x_set_reg8(rc52x,MFRC_REG_CommandReg, MFRC_Idle);
-
-	mfrc522_send(rc52x,MFRC_REG_FIFODataReg,buffer, 2);
-	rc52x_set_reg8(rc52x,MFRC_REG_CommandReg, MFRC_Transceive);
-
-	rc52x_or_reg8(rc52x,MFRC_REG_BitFramingReg, 0x80);
-
-
-	delay_ms(100);
-	rc52x_and_reg8(rc52x,MFRC_REG_BitFramingReg, ~0x80);
-
-	uint8_t error;
-	rc52x_get_reg8(rc52x, MFRC_REG_ErrorReg, &error, 1);
-	// Well... this is what the lib did, looks kinda weird
-	// But for a quick test, let's go ahead. We'll need to look
-	// at a proper implementation anyways...
-	memset(buffer,0,10);
-
-	if (! (error & 0x1B) ) {
-		uint8_t size;
-		rc52x_get_reg8(rc52x, MFRC_REG_FIFOLevelReg, &size, 1);
-		if (size > 10) size = 10;
-		mfrc522_recv(rc52x,MFRC_REG_FIFODataReg, buffer, size);
-		return 0;
+	serNum[0] = PICC_CMD_SEL_CL1;
+	serNum[1] = 0x20;
+	status = MFRC522_ToCard(rc52x, MFRC_Transceive, serNum, 2, serNum, &unLen);
+	if (status == MI_OK) {
+		// Check card serial number
+		for (i = 0; i < 4; i++) serNumCheck ^= serNum[i];
+		if (serNumCheck != serNum[i]) status = MI_ERR;
 	}
-	return -1;
+	return status;
 }
